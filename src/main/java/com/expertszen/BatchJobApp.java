6afb68838d44b51f4f2b4d1f2059457f724d7215
shreenathraval5 <shreenathraval5@gmail.com>
2025-11-03 20:21:00 +0530

    Update BatchJobApp.java

--- src/main/java/com/expertszen/BatchJobApp.java
+++ src/main/java/com/expertszen/BatchJobApp.java
@@ -2,6 +2,6 @@ package com.expertszen;
 
 public class BatchJobApp {
     public static void main(String[] args) {
-        System.out.println("Executing Java Batch Job Example!");
+        System.out.println("Executing Java Batch Job Example!")
     }
 }
